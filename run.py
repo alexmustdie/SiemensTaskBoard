288f75e3afdd69068bd3badbce63c0a1bb1b8af2
Aleksei Kovalenko <a.kovalenko@geoscan.aero>
2019-04-23 14:44:56 +0300

    add more logic and fix some bugs

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-import enum
 import os
+import enum
 
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -12,6 +12,8 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
     tasks = db.relationship('Task', backref='user', lazy=True)
 
 
@@ -24,54 +26,123 @@ class TaskStatus(enum.Enum):
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    status = db.Column(db.Enum(TaskStatus), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    status = db.Column(db.Enum(TaskStatus), nullable=False, default=TaskStatus.TODO)
 
 
 db.create_all()
 db.session.commit()
 
 
+@app.errorhandler(Exception)
+def handle_exception(exc):
+    return jsonify(error=str(exc))
+
+
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return 'Siemens Task Board'
 
 
 @app.route('/api/users.add', methods=['GET'])
 def add_user():
-    user = User()
+
+    first_name = request.args.get('first_name')
+    last_name = request.args.get('last_name')
+
+    user = User(first_name=first_name, last_name=last_name)
+
     db.session.add(user)
     db.session.commit()
-    return jsonify({'response': user.id})
+
+    return jsonify(id=user.id)
+
+
+@app.route('/api/users.edit', methods=['GET'])
+def edit_user():
+
+    user_id = int(request.args['user_id'])
+    first_name = request.args.get('first_name')
+    last_name = request.args.get('last_name')
+
+    user = db.session.query(User).get(user_id)
+
+    if first_name:
+        user.first_name = first_name
+
+    if last_name:
+        user.last_name = last_name
+
+    db.session.commit()
+
+    return jsonify(result=True)
 
 
 @app.route('/api/users.assignTask', methods=['GET'])
 def assign_task():
-    user_id = request.args.get('user_id')
-    task_id = request.args.get('task_id')
+
+    user_id = int(request.args['user_id'])
+    task_id = int(request.args['task_id'])
+
+    user = db.session.query(User).get(user_id)
     task = db.session.query(Task).get(task_id)
-    task.user_id = user_id
+
+    task.user_id = user.id
     db.session.commit()
-    return jsonify({'response': 1})
+
+    return jsonify(result=True)
 
 
 @app.route('/api/tasks.add', methods=['GET'])
 def add_task():
-    user_id = request.args.get('user_id')
-    task = Task(user_id=user_id, status=TaskStatus.TODO)
+
+    title = request.args['title']
+    description = request.args['description']
+    user_id = int(request.args.get('user_id') or 0)
+
+    user = db.session.query(User).get(user_id)
+    task = Task(title=title, description=description, user_id=user.id if user else 0)
+
     db.session.add(task)
     db.session.commit()
-    return jsonify({'response': task.id})
+
+    return jsonify(id=task.id)
+
+
+@app.route('/api/tasks.edit', methods=['GET'])
+def edit_task():
+
+    task_id = int(request.args['task_id'])
+    title = request.args.get('title')
+    description = request.args.get('description')
+
+    task = db.session.query(Task).get(task_id)
+
+    if title:
+        task.title = title
+
+    if description:
+        task.description = description
+
+    db.session.commit()
+
+    return jsonify(result=True)
 
 
 @app.route('/api/tasks.setStatus', methods=['GET'])
 def set_task_status():
-    task_id = request.args.get('task_id')
-    status = request.args.get('status')
+
+    task_id = int(request.args['task_id'])
+    status = int(request.args['status'])
+
     task = db.session.query(Task).get(task_id)
-    task.status = TaskStatus(int(status))
+    task.status = TaskStatus(status)
+
     db.session.commit()
-    return jsonify({'response': 1})
+
+    return jsonify(result=True)
 
 
 if __name__ == '__main__':
